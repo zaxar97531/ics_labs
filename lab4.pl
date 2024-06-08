6b6f6c881871ab1b89926c41f55c9335359db652
zaxar97531 <116964410+zaxar97531@users.noreply.github.com>
2024-06-08 11:41:57 +0800

    Update lab4.pl

--- lab4.pl
+++ lab4.pl
@@ -1,141 +1,70 @@
-/** ИГРЫ */
-game('The Witcher 3: Wild Hunt', 'CD Projekt Red', 2015, 'action', 'RPG', 300, 'Poland', 'Geralt of Rivia', 9.3, 252).
-game('The Legend of Zelda: Breath of the Wild', 'Nintendo', 2017, 'action-adventure', 'fantasy', 150, 'Japan', 'Link', 9.5, 153).
-game('Grand Theft Auto V', 'Rockstar North', 2013, 'action-adventure', 'open world', 100, 'United Kingdom', 'Various', 9.6, 730).
-game('The Last of Us Part II', 'Naughty Dog', 2020, 'action-adventure', 'survival horror', 40, 'USA', 'Ellie', 9.0, 102).
-game('Red Dead Redemption 2', 'Rockstar Games', 2018, 'action-adventure', 'open world', 60, 'USA', 'Arthur Morgan', 9.8, 427).
-game('Dark Souls III', 'FromSoftware', 2016, 'action', 'RPG', 50, 'Japan', 'Various', 8.6, 104).
-game('Super Mario Odyssey', 'Nintendo', 2017, 'platformer', 'adventure', 20, 'Japan', 'Mario', 9.7, 351).
-game('Overwatch', 'Blizzard Entertainment', 2016, 'first-person shooter', 'multiplayer', 30, 'USA', 'Various', 7.6, 115).
-game('Portal 2', 'Valve Corporation', 2011, 'puzzle', 'first-person shooter', 15, 'USA', 'Chell', 9.5, 224).
-game('Minecraft', 'Mojang Studios', 2011, 'sandbox', 'adventure', 50, 'Sweden', 'Steve', 9.2, 743).
-game('League of Legends', 'Riot Games', 2009, 'MOBA', 'multiplayer', 'online', 'USA', 'Various', 8.4, 2112).
-game('The Elder Scrolls V: Skyrim', 'Bethesda Game Studios', 2011, 'action', 'RPG', 'open world', 80, 'USA', 'Various', 9.3, 487).
+% Database of games
+game('The Witcher 3: Wild Hunt', 'CD Projekt Red', 'RPG', 'action', 'PC', 'singleplayer', 300, 'Poland', 9.3).
+game('The Legend of Zelda: Breath of the Wild', 'Nintendo', 'fantasy', 'action-adventure', 'Nintendo Switch', 'singleplayer', 150, 'Japan', 9.5).
+game('Grand Theft Auto V', 'Rockstar North', 'open world', 'action-adventure', 'PC', 'singleplayer', 100, 'United Kingdom', 9.6).
+game('The Last of Us Part II', 'Naughty Dog', 'survival horror', 'action-adventure', 'PlayStation', 'singleplayer', 40, 'USA', 9.0).
+game('Red Dead Redemption 2', 'Rockstar Games', 'open world', 'action-adventure', 'PlayStation', 'singleplayer', 60, 'USA', 9.8).
+game('Dark Souls III', 'FromSoftware', 'RPG', 'action', 'PC', 'singleplayer', 50, 'Japan', 8.6).
+game('Super Mario Odyssey', 'Nintendo', 'adventure', 'platformer', 'Nintendo Switch', 'singleplayer', 20, 'Japan', 9.7).
+game('Overwatch', 'Blizzard Entertainment', 'multiplayer', 'first-person shooter', 'PC', 'multiplayer', 30, 'USA', 7.6).
+game('Portal 2', 'Valve Corporation', 'puzzle', 'first-person shooter', 'PC', 'singleplayer', 15, 'USA', 9.5).
+game('Minecraft', 'Mojang Studios', 'adventure', 'sandbox', 'PC', 'singleplayer', 50, 'Sweden', 9.2).
+game('League of Legends', 'Riot Games', 'multiplayer', 'MOBA', 'PC', 'online', 30, 'USA', 8.4).
+game('The Elder Scrolls V: Skyrim', 'Bethesda Game Studios', 'RPG', 'action', 'PC', 'singleplayer', 80, 'USA', 9.3).
 
-/** РАЗРАБОТЧИКИ */
-developer('CD Projekt Red').
-developer('Nintendo').
-developer('Rockstar North').
-developer('Naughty Dog').
-developer('Rockstar Games').
-developer('FromSoftware').
-developer('Blizzard Entertainment').
-developer('Valve Corporation').
-developer('Mojang Studios').
-developer('Riot Games').
-developer('Bethesda Game Studios').
+% Get platforms from user's input
+get_platform(Platform, X) :- Platform = pc, X = ['PC'].
+get_platform(Platform, X) :- Platform = playstation, X = ['PlayStation'].
+get_platform(Platform, X) :- Platform = xbox, X = ['Xbox'].
+get_platform(Platform, X) :- Platform = nintendo_switch, X = ['Nintendo Switch'].
+get_platform(Platform, X) :- Platform = mobile, X = ['Mobile'].
 
-/** ЖАНРЫ */
-genre('action').
-genre('action-adventure').
-genre('RPG').
-genre('platformer').
-genre('first-person shooter').
-genre('puzzle').
-genre('sandbox').
-genre('MOBA').
+% Get genres from user's input
+get_genre(Genre, X) :- Genre = action, X = ['action'].
+get_genre(Genre, X) :- Genre = adventure, X = ['action-adventure'].
+get_genre(Genre, X) :- Genre = rpg, X = ['RPG'].
+get_genre(Genre, X) :- Genre = platformer, X = ['platformer'].
+get_genre(Genre, X) :- Genre = shooter, X = ['first-person shooter'].
+get_genre(Genre, X) :- Genre = puzzle, X = ['puzzle'].
+get_genre(Genre, X) :- Genre = sandbox, X = ['sandbox'].
+get_genre(Genre, X) :- Genre = moba, X = ['MOBA'].
 
-/** ПЛАТФОРМЫ */
-platform('PC').
-platform('PlayStation').
-platform('Xbox').
-platform('Nintendo Switch').
-platform('Mobile').
+% Get modes from user's input
+get_mode(Mode, X) :- Mode = singleplayer, X = ['singleplayer'].
+get_mode(Mode, X) :- Mode = multiplayer, X = ['multiplayer'].
+get_mode(Mode, X) :- Mode = online, X = ['online'].
 
-/** ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ */
-game_with_developer(Name) :- game(Name, Developer, _, _, _, _, _, _, _, _), developer(Developer).
-game_with_genre(Name) :- game(Name, _, _, Genre, _, _, _, _, _, _), genre(Genre).
-game_with_platform(Name) :- game(Name, _, _, _, _, _, _, _, _, _), platform(Name).
+% Check if X is an element of List
+elem(X, [X|_]).
+elem(X, [_|Tail]) :- elem(X, Tail).
 
-/** СИСТЕМА РЕКОМЕНДАЦИЙ ДЛЯ ИГР */
-recommendation(X, Y, O) :-
-    (X == 'PC' ; X == 'PlayStation' ; X == 'Xbox' ; X == 'Nintendo Switch' ; X == 'Mobile'),
-    (Y == 'action' ; Y == 'adventure' ; Y == 'RPG' ; Y == 'platformer' ; Y == 'shooter' ; Y == 'puzzle' ; Y == 'sandbox' ; Y == 'MOBA'),
-    (O == 'singleplayer' ; O == 'multiplayer' ; O == 'online'),
-    game(Name, _, _, Y, _, Duration, _, _, _, _),
-    platform(Name, X),
-    ((Duration < 50, O == 'singleplayer') ; (Duration < 30, O == 'multiplayer') ; (O == 'online')),
-    !,
-    write(Name).
+% Check whether lists have a common element
+elems(X, [Y|_]) :- elem(Y, X).
+elems(X, [_|Tail]) :- elems(X, Tail).
 
-/** ОСНОВНАЯ ЛОГИКА ПРОГРАММЫ */
-start :-
-    write('Экспертная система - Рекомендатель игр'), nl,
-    write('Ответьте на следующие вопросы для получения рекомендации игр.'), nl,
-    write('На какой платформе вы хотели бы играть? '), read(Platform), nl,
-    write('Какой жанр игр вас интересует? '), read(Genre), nl,
-    write('Вы предпочитаете одиночные игры, многопользовательские или онлайн? '), read(Mode), nl,
-    write('Рекомендуемые игры: '), nl,
-    recommendation(Platform, Genre, Mode), nl.
+% List games that match the criteria
+list_game(Developer, Genres, Platforms, Modes, MaxDuration, Acc, L) :-
+    game(Name, Dev, Genre, _, Platform, Mode, Duration, _, _),
+    elem(Dev, Developer),
+    elem(Genre, Genres),
+    elem(Platform, Platforms),
+    elem(Mode, Modes),
+    Duration =< MaxDuration,
+    \+ elem(Name, Acc), !,
+    list_game(Developer, Genres, Platforms, Modes, MaxDuration, [Name|Acc], L).
 
-/** СИСТЕМА РЕКОМЕНДАЦИЙ ДЛЯ ФИЛЬМОВ */
-recommendation_year(Y, Ans1, Ans2, Ans3, Ans4, Ans5, Ans6) :-
-    film(Y, Ans1, Ans2, Ans3, Ans4, Ans5, Ans6).
+list_game(_, _, _, _, _, L, L).
 
-film_with_genre(Y, Gendre) :-
-    film(Y, _, Gendre, _, _, _, _).
-
-/** ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ ДЛЯ ФИЛЬМОВ И ИГР */
-game_with_developer(Name) :- game(Name, Developer, _, _, _, _, _, _, _, _), developer(Developer).
-game_with_genre(Name) :- game(Name, _, _, Genre, _, _, _, _, _, _), genre(Genre).
-game_with_platform(Name) :- game(Name, _, _, _, _, _, _, _, _, _), platform(Name).
-
-recommendation_platform(Platform, Name) :-
-    game(Name, _, _, _, _, _, _, _, _, _),
-    platform(Name, Platform).
-
-recommendation_genre(Genre, Name) :-
-    game(Name, _, _, Genre, _, _, _, _, _, _).
-
-recommendation_mode(Mode, Name) :-
-    game(Name, _, _, _, _, Duration, _, _, _, _),
-    ((Duration < 50, Mode == 'singleplayer') ; (Duration < 30, Mode == 'multiplayer') ; (Mode == 'online')).
-
-/** ОСНОВНАЯ ЛОГИКА ПРОГРАММЫ */
-start :-
-    write('Экспертная система - Рекомендатель фильмов и игр'), nl,
-    write('Выберите, что вы хотели бы получить рекомендации:'), nl,
-    write('1. Фильмы'), nl,
-    write('2. Игры'), nl,
-    read(Choice), nl,
-    (
-        Choice == 1 ->
-            write('Ответьте на следующие вопросы для получения рекомендации фильма.'), nl,
-            write('Предпочитаете ли вы старые фильмы? '), read(A1), nl,
-            (   A1 == 'no' ->
-                write('Предпочитаете ли вы новые фильмы? '), read(A2), nl,
-                (   A2 == 'no' -> Ans1 = 'decent' ; Ans1 = 'newer' )
-            ;   Ans1 = 'older'
-            ),
-            write('Какой жанр фильма вас интересует? '), read(Ans2), nl,
-            (
-                Ans2 == 'horror' ->
-                write('Чего вы боитесь? '), read(Ans5), nl
-                ; Ans2 == 'adventure' ->
-                write('Какого типа приключенческие фильмы вас интересуют? '), read(Ans5), nl
-                ; Ans5 = 'others'
-            ),
-            write('Сколько времени у вас есть? '), read(C1), nl,
-            (
-                C1 == 'many' ->
-                write('У вас действительно много времени? '), read(C2), nl,
-                (   C2 == 'no' -> Ans3 = 'long' ; Ans3 = 'verylong' )
-                ; Ans3 = 'short'
-            ),
-            write('Понравилось ли вам польское кино? '), read(Ans4), nl,
-            write('Главный герой - мужчина? '), read(E1), nl,
-            (   E1 == 'yes' -> Ans6 = 'm' ; (E1 == 'no' -> Ans6 = 'w' ; Ans6 = '') ),
-            recommendation_year(Y, Ans1, Ans2, Ans3, Ans4, Ans5, Ans6),
-            write('Рекомендуемый жанр фильма: '), write(Y), nl
-        ;   Choice == 2 ->
-            write('Ответьте на следующие вопросы для получения рекомендации игр.'), nl,
-            write('На какой платформе вы хотели бы играть? '), read(Platform), nl,
-            write('Какой жанр игр вас интересует? '), read(Genre), nl,
-            write('Вы предпочитаете одиночные игры, многопользовательские или онлайн? '), read(Mode), nl,
-            write('Рекомендуемые игры: '), nl,
-            recommendation_platform(Platform, Name),
-            recommendation_genre(Genre, Name),
-            recommendation_mode(Mode, Name),
-            !,
-            write(Name), nl
-    ).
+% Suggest game to a user
+suggest_game(L) :- 
+    write('Welcome to the Game Recommendation System!'), nl,
+    write('Please answer the following questions to get a game recommendation.'), nl,
+    write('Choose your platform: (pc, playstation, xbox, nintendo_switch, mobile)'), nl,
+    read(Platform),
+    write('Choose the genre you are interested in: (action, adventure, rpg, platformer, shooter, puzzle, sandbox, moba)'), nl,
+    read(Genre),
+    write('Do you prefer singleplayer, multiplayer, or online games?'), nl,
+    read(Mode),
+    write('What is the maximum duration of the game in hours?'), nl,
+    read(Duration),
+    list_game(_, [Genre], [Platform], [Mode], Duration, [], L).
